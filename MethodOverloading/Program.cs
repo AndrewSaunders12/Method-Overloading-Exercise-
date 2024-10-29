e7dec8b79b8c536a9b9b5f849dd66cf9ba6d15fe
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-10-29 18:10:21 -0400

    Method Overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(7, 14, true));
         }
+
+
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        static string Add(int num1, int num2, bool isTrue)
+        {
+            if (isTrue)
+            {
+                return num1 + num2 + " dollars" ;
+            }
+            else
+            {
+                return "Nuh uh";
+            }
+        }
+
     }
+
 }
